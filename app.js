84fe07d182640c2469f0de27616cd255ad8ba606
Jerry Ogunbor <ogunborjerry@gmail.com>
2022-08-04 10:30:50 +0100

    Created the .env file with the mongourl string and added the code to app.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-// Basic express server
+require('dotenv').config();
 
-const express = require('express');
-const app = express()
+// express
+const express = require('express')
+const app = express();
+
+// database
+const connectDB = require('./db/connect');
 
 
-const port = process.env.PORT || 5000
-const start = async => {
+const port = process.env.PORT || 5000;
+const start = async () => {
     try {
-       app.listen(port, console.log(`server listening on port ${port}`))
+       await connectDB(process.env.MONGO_URL);
+       app.listen(port, console.log(`server listening on port ${port}...`))
     } catch (error) {
        console.log(error);
     }
 }
-
 
 start()
